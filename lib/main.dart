182bf3a5ce7f0b2a01e5e4cc15a9d2d9f377baec
= <=>
2023-12-06 20:18:48 +0200

    Register note model adapter

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:notes/Models/note_model.dart';
+import 'package:notes/constants/strings.dart';
 import 'package:notes/notes_app.dart';
 
-void main(){
+void main() async{
+  await Hive.initFlutter();
+  await Hive.openBox(kNotesBox);
+  Hive.registerAdapter(NotelModelAdapter());
   runApp(const NotesApp());
 }
