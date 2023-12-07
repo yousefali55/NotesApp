1febff338e30f744ad64c45962859acb0efa2084
= <=>
2023-12-07 14:47:42 +0200

    Add SimpleBlocObsserver

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:notes/Models/note_model.dart';
 import 'package:notes/constants/strings.dart';
 import 'package:notes/notes_app.dart';
+import 'package:notes/simple_bloc_observer.dart';
 
 void main() async{
+  Bloc.observer = SimpleBlocObserver();
   await Hive.initFlutter();
   await Hive.openBox(kNotesBox);
   Hive.registerAdapter(NotelModelAdapter());
