a05565db0a50924258bd09d267e39f6bf54e579b
= <=>
2023-12-07 13:59:37 +0200

    Add BlocProvider

--- lib/notes_app.dart
+++ lib/notes_app.dart
@@ -1,16 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:notes/cubits/add_note/add_note_cubit.dart';
 import 'package:notes/views/notes.dart';
+
 class NotesApp extends StatelessWidget {
   const NotesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  ScreenUtilInit(designSize: Size(375, 812),
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData.dark(),
-        home: NotesScreen(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => AddNoteCubit(),
+        ),
+      ],
+      child: ScreenUtilInit(
+        designSize: Size(375, 812),
+          child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData.dark(),
+            home: NotesScreen(),
+          ),
       ),
     );
   }
